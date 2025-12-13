8f0e78b412383a4a3692705f3fddbf3e3c49e028
Your Name <your_email@gmail.com>
2025-12-13 21:25:52 +0600

    Test Railway MySQL connection with SSL

--- test-db.js
+++ test-db.js
@@ -5,7 +5,11 @@ const db = mysql.createConnection({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  port: Number(process.env.DB_PORT)
+  port: Number(process.env.DB_PORT),
+  ssl: {
+    rejectUnauthorized: false
+  },
+  connectTimeout: 10000   // 10 seconds timeout
 });
 
 db.connect((err) => {
